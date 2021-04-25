35aff9972e42929129102c64db1c406078d015b9
Pradeep Elamurugu <56883812+pradeepelamurugu@users.noreply.github.com>
2021-04-26 01:08:36 +0530

    Update urls.py

--- HostelManagementSystem/urls.py
+++ HostelManagementSystem/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('student.urls')),
-    path('favicon\.ico$',RedirectView.as_view(url='/static/images/favicon.ico')),
+ 
 
 ]
